35cb1a5101b483eebef3a40cf636238fafe559cf
monicah pirisi <monicah.lekupe@ashesi.edu.gh>
2025-11-06 17:12:18 +0000

    updates

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -5,6 +5,11 @@
  * Please use settings/config.php which loads from .env file
  */
 
+if (!defined('SERVER'))   define('SERVER', 'localhost');
+if (!defined('USERNAME')) define('USERNAME', 'root');
+if (!defined('PASSWD'))   define('PASSWD', '');
+if (!defined('DATABASE')) define('DATABASE', 'ecommerce_2025A_monicah_lekupe');
+
 // Load configuration from .env file
 require_once __DIR__ . '/config.php';
 
