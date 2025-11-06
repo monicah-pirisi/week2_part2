3f578976ff844fc5ff6d15f5e79f3d9c09968e0d
monicah pirisi <monicah.lekupe@ashesi.edu.gh>
2025-11-06 18:21:07 +0000

    updates

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -6,12 +6,12 @@
  */
 
 if (!defined('SERVER'))   define('SERVER', 'localhost');
-if (!defined('USERNAME')) define('USERNAME', 'root');
-if (!defined('PASSWD'))   define('PASSWD', '');
+if (!defined('USERNAME')) define('USERNAME', 'monicah.lekupe');
+if (!defined('PASSWD'))   define('PASSWD', 'Amelia@2026');
 if (!defined('DATABASE')) define('DATABASE', 'ecommerce_2025A_monicah_lekupe');
 
 // Load configuration from .env file
-require_once __DIR__ . '/config.php';
+//require_once __DIR__ . '/config.php';
 
 // Constants are already defined in config.php
 // No need to redefine them here
